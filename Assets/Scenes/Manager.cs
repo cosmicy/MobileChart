c9d9dadb1bd487d72f343d20eb2bf30653eeb66a
chengyuan <tdqs_cy@163.com>
2018-07-26 15:10:04 +0800

    尝试页面透明

---
+++ Assets/Scenes/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		_webView = GameObject.Find("UniWebView").GetComponent<UniWebView>();
+		_webView.BackgroundColor = new Color(1, 1, 1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	UniWebView _webView;
+
+
+}
